23834fd9929c69ab2965d7206e3d3bd078edab83
Satyam Seth <satyam1998.1998@gmail.com>
2025-04-20 12:59:07 +0530

    Add basic fastapi server

--- try uv/chai-code/main.py
+++ try uv/chai-code/main.py
@@ -1,6 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
 def main():
     print("Hello from chai-code!")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Chai Code!"}
 
 if __name__ == "__main__":
     main()
